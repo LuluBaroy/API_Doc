6bc74e781fbfc2ca1769a2267230815d3193f89f
Lulu Baroy <lucie.baroiller@gmail.com>
2020-07-19 00:14:47 +0200

    Init Commit - API DOC

---
+++ api_project.js
@@ -0,0 +1,19 @@
+define({
+  "name": "API So Pekocko",
+  "version": "0.1.0",
+  "description": "Projet 6 - OpenClassRooms - Lucie BAROILLER",
+  "title": "So Pekocko",
+  "order": [
+    "User",
+    "Sauces"
+  ],
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-07-18T22:13:12.692Z",
+    "url": "http://apidocjs.com",
+    "version": "0.23.0"
+  }
+});
